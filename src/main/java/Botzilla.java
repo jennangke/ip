b6774bdae75f3c54baa460db8652b35a08b51cd7
jennangke <jennangke@gmail.com>
2026-08-20 16:10:20 +0800

    echo

--- src/main/java/Botzilla.java
+++ src/main/java/Botzilla.java
@@ -1,18 +1,36 @@
+import java.util.Scanner;
+
 public class Botzilla {
     public static void main(String[] args) {
         String banner =
                 "    ____        __        _ ____     \n" +
-                "   / __ )____  / /_____  (_) / /___ _\n" +
-                "  / __  / __ \\/ __/_  / / / / / __ `/\n" +
-                " / /_/ / /_/ / /_  / /_/ / / / /_/ / \n" +
-                "/_____/\\____/\\__/ /___/_/_/_/\\__,_/  \n";
+                        "   / __ )____  / /_____  (_) / /___ _\n" +
+                        "  / __  / __ \\/ __/_  / / / / / __ `/\n" +
+                        " / /_/ / /_/ / /_  / /_/ / / / /_/ / \n" +
+                        "/_____/\\____/\\__/ /___/_/_/_/\\__,_/  \n";
 
         System.out.println("____________________________________________________________");
         System.out.println(banner);
         System.out.println("Hello there! I'm Botzilla.");
         System.out.println("What can I do for you on this fine day?");
         System.out.println("____________________________________________________________");
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            String input = scanner.nextLine();
+
+            if (input.equals("bye")) {
+                break;
+            }
+
+            System.out.println(input);
+            System.out.println("____________________________________________________________");
+        }
+
         System.out.println("Cheers! Have a great day!");
         System.out.println("____________________________________________________________");
+
+        scanner.close();
     }
 }
